a69bf91df21fbed948e507fb480ab1afd5d42ecf
Valerie Obenchain <vobencha@fhcrc.org>
2011-06-12 21:03:30 +0000

    Edits to locateVariants and work on the vignette.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/VariantAnnotation@56123 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/locateVariants.R
+++ R/locateVariants.R
@@ -1,15 +1,24 @@
-locateVariants <- function(query, subject, orgPackage, ...)
+locateVariants <- function(query, subject, ...)
 {
+t1 <- proc.time()
     chrom <- unique(seqlevels(query))
-    exons <- exons(subject, vals=list(exon_chrom=chrom),
-       columns=c("exon_id", "tx_id", "gene_id"))
+    #exons <- exons(subject, vals=list(exon_chrom=chrom),
+    #   columns=c("exon_id", "tx_id", "gene_id"))
+    cdsByTx <- cdsBy(subject)
     tx <- transcripts(subject, vals=list(exon_chrom=chrom),
        columns=c("exon_id", "tx_id", "gene_id"))
-    exFO <- findOverlaps(query, exons)
+    #exFO <- findOverlaps(query, exons)
+    cdsFO <- findOverlaps(query, cdsByTx, type="within")
     txFO <- findOverlaps(query, tx)
-    exCO <- tabulate(queryHits(exFO), length(query))
+    #exCO <- tabulate(queryHits(exFO), length(query))
+    cdsCO <- tabulate(queryHits(cdsFO), length(query))
     txCO <- tabulate(queryHits(txFO), length(query))
+cat("set up", proc.time() - t1,"\n")
 
+    ## exon :
+    exonic <- cdsCO > 0 
+
+t2 <- proc.time()
     ## intergenic :
     intergenic <- txCO == 0
     intvar <- query[txCO == 0]
@@ -18,51 +27,43 @@ locateVariants <- function(query, subject, orgPackage, ...)
             values(c(tx[precede(intvar[x,], tx)], 
             tx[follow(intvar[x,], tx)]))[["gene_id"]] 
         }, tx)
+cat("intergenic",proc.time() - t2,"\n")
 
-    ## intronic :
-    intronic <- txCO != 0 & exCO == 0
+    ## intron :
+    intronic <- txCO != 0 & cdsCO == 0
 
+t4 <- proc.time()
     ## UTRs :
     fiveUTR <- fiveUTRsByTranscript(subject)
     threeUTR <- threeUTRsByTranscript(subject)
     utr5 <- countOverlaps(query, fiveUTR) > 0
     utr3 <- countOverlaps(query, threeUTR) > 0
+cat("utr",proc.time() - t4,"\n")
 
-    ## exonic :
-    exonic <- countOverlaps(query, cdsBy(subject), type="within") > 0 
-    #exonic <- exCO > 0 & utr5 == FALSE & utr3 == FALSE
 
-    ## gene metadata
+t5 <- proc.time()
+    ## geneID 
     genes <- character(length(query)) 
+    transcripts <- character(length(query)) 
     genes[txCO == 0] <- flankGenes
     genes[txCO != 0] <- split(values(tx)[["gene_id"]][subjectHits(txFO)], queryHits(txFO))
+    transcripts[txCO != 0] <- split(values(tx)[["tx_id"]][subjectHits(txFO)], queryHits(txFO))
     genes <- lapply(genes, function(x) unique(as.numeric(x)))
-    len <- unlist(lapply(genes, length), use.names=FALSE)
+    transcripts <- lapply(transcripts, function(x) unique(as.numeric(x)))
+    geneID <- CharacterList(genes)
+    txID <- CharacterList(transcripts)
+cat("geneID",proc.time() - t5,"\n")
 
-    ugenes <- unlist(genes, use.names=FALSE)
-    x <- org.Hs.egSYMBOL
-    mapped_symbols <- mappedkeys(x)
-    xx <- as.list(x[mapped_symbols])
-    gsymbol <- xx[names(xx) %in% ugenes]
-    gsymbol <- gsymbol[match(ugenes, names(gsymbol))]
-    #x <- org.Hs.egGENENAME
-    #mapped_genes <- mappedkeys(x)
-    #xx <- as.list(x[mapped_genes])
-    #gname <- xx[names(xx) %in% ugenes]
-    #gname <- gname[match(ugenes, names(gname))]
-    #geneInfo <- unlist(paste(gsymbol, ": ", 
-    #    gname[match(ugenes, names(gname))], sep=""), use.names=FALSE)
-    genes <- CharacterList(split(gsymbol, rep(seq_len(length(len)), len)))
+    ## FIXME : report all matches or by priority?
 
-    ## FIXME : priority if >1 category=TRUE
     location <- character(length(query))
-    location[exonic == TRUE] <- "exonic"
-    location[intronic == TRUE] <- "intronic"
+    location[exonic == TRUE] <- "exon"
+    location[intronic == TRUE] <- "intron"
     location[intergenic == TRUE] <- "intergenic"
     location[utr5 == TRUE] <- "5'UTR"
     location[utr3 == TRUE] <- "3'UTR"
 
-    values(query) <- append(values(query), DataFrame(location, genes))
+    values(query) <- append(values(query), DataFrame(location, txID, geneID))
     query
 }
 
