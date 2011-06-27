d34e1a337c1946b317af4f3d352635332988b9da
Valerie Obenchain <vobencha@fhcrc.org>
2011-06-27 22:12:54 +0000

    predictCoding - make use of DNAString and AAString sets for in put and output - return a DataFrame instead of subset instance of the query, the index   in the DataFrame can be used to trace back to the ranges in the query
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/VariantAnnotation@56447 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/AllGenerics.R
+++ R/AllGenerics.R
@@ -1,5 +1,5 @@
 setGeneric("predictCoding", signature = c("query", "subject"),
-           function(query, subject, variantAllele, seqSource, ...)
+           function(query, subject, seqSource, refAllele, varAllele, ...)
            standardGeneric("predictCoding")
 )
 
@@ -7,3 +7,16 @@ setGeneric("getTranscriptSeqs", signature = c("query", "subject"),
            function(query, subject, ...)
            standardGeneric("getTranscriptSeqs")
 )
+
+
+### SRFilter
+#
+#setGeneric("name", function(x, ...) standardGeneric("name"))
+#
+#setGeneric("stats", function(x, ...) standardGeneric("stats"))
+#
+#setGeneric("srFilter", function(fun, name=NA_character_, ...)
+#           standardGeneric("srFilter"),
+#           signature="fun")
+#
+
--- R/predictCoding-methods.R
+++ R/predictCoding-methods.R
@@ -1,43 +1,44 @@
 setMethod("predictCoding",  c("Ranges", "TranscriptDb"),
-    function(query, subject, alleles, seqSource, ...)
+    function(query, subject, seqSource, refAllele, varAllele, ...)
     {
         cdsByTx <- cdsBy(subject)
         x <- as(query, "GRanges")
-        callGeneric(query=x, subject=cdsByTx, alleles=alleles,
-            seqSource=seqSource, ...) 
+        callGeneric(query=x, subject=cdsByTx, seqSource=seqSource, 
+            refAllele=refAllele, varAllele=varAllele, ...) 
     }
 )
 
 setMethod("predictCoding",  c("GRanges", "TranscriptDb"),
-    function(query, subject, alleles, seqSource, ...)
+    function(query, subject, seqSource, refAllele, varAllele, ...)
     {
         cdsByTx <- cdsBy(subject)
-        callGeneric(query=query, subject=cdsByTx, alleles=alleles,
-            seqSource=seqSource, ...) 
+        callGeneric(query=query, subject=cdsByTx, seqSource=seqSource, 
+            refAllele=refAllele, varAllele=varAllele, ...) 
     }
 )
 
 setMethod("predictCoding", c("Ranges", "GRangesList"),
-    function(query, subject, alleles, seqSource, ...)
+    function(query, subject, seqSource, refAllele, varAllele, ...)
     {
         x <- as(query, "GRanges")
-        callGeneric(query=x, subject=subject, alleles=alleles,
-            seqSource=seqSource, ...) 
+        callGeneric(query=x, subject=subject, seqSource=seqSource, 
+            refAllele, varAllele, ...) 
     }
 )
 
 setMethod("predictCoding", c("GRanges", "GRangesList"),
-    function(query, subject, alleles, seqSource, ...)
+    function(query, subject, seqSource, refAllele, varAllele, ...)
     {
         ## FIXME : findOverlaps is done here, globalToLocal and locateVariants
-        fo <- findOverlaps(query, subject, type = "within")
+        ## adjust query start for width=0
+        queryAdj <- query
+        start(queryAdj[width(query) == 0]) <- 
+            start(query)[width(query) == 0] - 1 
+        fo <- findOverlaps(queryAdj, subject, type = "within")
         subject <- subject[unique(subjectHits(fo))]
         txSeqs <- getTranscriptSeqs(subject, seqSource)
-        txLocal <- globalToLocal(query, subject)
+        txLocal <- globalToLocal(queryAdj, subject)
         xCoding <- query[txLocal$global.ind]
-        splitAlleles <- do.call(rbind, strsplit(values(xCoding)[[alleles]], "/"))
-        refAlleles <- splitAlleles[,1] 
-        varAlleles <- splitAlleles[,2] 
 
         ## FIXME : check original sequence from BSgenome/fasta
         ##         against user provided refAllele?
@@ -47,71 +48,37 @@ setMethod("predictCoding", c("GRanges", "GRangesList"),
         codonStart <- (start(txLocal$local) - 1L) %/% 3L * 3L + 1L
         codonEnd <- codonStart + 
             originalWidth %/% 3L * 3L + 2L
-        codons <- tolower(substring(txSeqs[txLocal$ranges.ind], codonStart,
-            codonEnd))
-	#substring(codons, varPosition, varPosition) <- 
-	#    toupper(substring(codons, varPosition, varPosition)) 
+        codons <- DNAStringSet(substring(txSeqs[txLocal$ranges.ind], 
+            codonStart, codonEnd))
 
         ## variant codons
-        varWidth <- nchar(varAlleles)
+        varWidth <- width(values(xCoding)[[varAllele]]) 
         varPosition <- (start(txLocal$local) - 1L) %% 3L + 1L
-        insertions <- refAlleles == "-" 
-        deletions <- varAlleles == "-" 
-        indels <- insertions | deletions
-        allowedSubstitutions <- !indels &
-            abs(varWidth - originalWidth) %% 3L == 0
-        allowedIndels <- indels  &
-            (abs(varWidth - originalWidth) + 1L) %% 3L == 0
-        translateIdx <- allowedSubstitutions | allowedIndels 
+        indels <- originalWidth == 0 | varWidth == 0 
+        translateIdx <- abs(varWidth - originalWidth) %% 3 == 0 
         varCodons <- codons
-
-
-	## FIXME : simplify
-        ## substitutions, including snvs
-        first <- substr(varCodons[!indels], 1,
-            varPosition[!indels] - 1L)
-        last <- substr(varCodons[!indels], varPosition[!indels] + 1L,
-            nchar(varCodons[!indels]))
-        varCodons[!indels] <- paste(first, varAlleles[!indels], last, sep="")
-        ## insertions
-        first <- substr(varCodons[insertions], varPosition[insertions] - 1L,
-            varPosition[insertions] - 1L)
-        last <- substr(varCodons[insertions], varPosition[insertions],
-            nchar(varCodons[insertions]))
-        varCodons[insertions] <- paste(first, varAlleles[insertions], last, sep="")
-        ## deletions
-        first <- substr(varCodons[deletions], 1, varPosition[deletions] - 1L)
-        last <- substr(varCodons[deletions], varPosition[deletions] +
-            originalWidth[deletions], nchar(varCodons[deletions]))
-        varCodons[deletions] <- paste(first, last, sep="")
-	
+        subseq(varCodons, start=varPosition, width=originalWidth) <- 
+            values(xCoding)[[varAllele]]
 
         ## results
-        values(xCoding)[colnames(values(subject@unlistData))] <-
+        queryIndex <- txLocal$global.ind
+        tx_id <- names(subject)[txLocal$ranges.ind]
+        fromSubject <-
             values(subject@unlistData)[txLocal$ranges.ind,]
-        values(xCoding)$tx_ID <- names(subject)[txLocal$ranges.ind]
-
-        CodonChange <- paste(codons, "/",
-            varCodons, sep="") 
-        CodonChange[!translateIdx] <- NA
-        values(xCoding)$CodonChange <- CodonChange 
-
-        refAA <- translate(DNAStringSet(codons))
-        varAA <- NA 
-        varAA[translateIdx] <-
-            as.character(translate(DNAStringSet(varCodons)[translateIdx]))
-        AAChange <- paste(refAA, "/", varAA, sep="")
-        AAChange[!translateIdx] <- NA 
-        values(xCoding)$AAChange <- AAChange
-
+        refCodon <- codons
+        varCodon <- varCodons
+        refAA <- translate(codons)
+        varAA <- AAStringSet(rep("", length(queryIndex))) 
+        varAA[translateIdx] <- translate(varCodons[translateIdx])
+ 
         ## FIXME : better check for equality
-        nonsynonymous <- as.character(refAA) != varAA 
+        nonsynonymous <- as.character(refAA) != as.character(varAA) 
         Consequence <- rep("synonymous", length(xCoding))
         Consequence[nonsynonymous] <- "nonsynonymous" 
         Consequence[!translateIdx] <- "frameshift" 
-        values(xCoding)$Consequence <- Consequence 
         
-        xCoding
+        DataFrame(queryIndex, tx_id, refCodon, varCodon, 
+            refAA, varAA, Consequence, fromSubject) 
     }
 )
 
