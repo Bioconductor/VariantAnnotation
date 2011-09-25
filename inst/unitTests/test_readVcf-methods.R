3da324ed916e98779c22517453501ede9e8e83d0
Valerie Obenchain <vobencha@fhcrc.org>
2011-09-25 02:25:48 +0000

    - added unit tests for readVcf - updated saved GRanges instance
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/VariantAnnotation@58358 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ inst/unitTests/test_readVcf-methods.R
@@ -0,0 +1,11 @@
+f1 <- system.file("extdata", "ex1.vcf", package="VariantAnnotation")
+f2 <- system.file("extdata", "ex2.vcf", package="VariantAnnotation")
+
+test_readVcf <- function()
+{
+    vcf <- readVcf(f1)
+    checkIdentical(names(assays(vcf)), c("GT", "GQ", "DP"))
+    checkEquals(width(rowData(vcf)), width(values(rowData(vcf))[["REF"]])) 
+    checkEquals(nrow(unlist(values(rowData(vcf))[["ALT"]])), 18) 
+    checkEquals(scanVcf(f1)[[1]]$POS, start(rowData(vcf))) 
+}
