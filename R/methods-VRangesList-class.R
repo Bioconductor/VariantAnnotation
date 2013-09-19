054aa0a203ca38ffb7ce04cc48e535a3b04ae751
Michael Lawrence <lawremi@iastate.edu>
2013-09-19 13:30:54 +0000

    got rid of merge,VRangesList; did not make that much sense in retrospect; might be reincarnated as something else some day.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/VariantAnnotation@80579 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/methods-VRangesList-class.R
+++ R/methods-VRangesList-class.R
@@ -13,20 +13,3 @@ setMethod("stackSamples", "VRangesList", function(x) {
     sampleNames(stacked) <- Rle(names(x), elementLengths(x))
   stacked
 })
-
-setMethod("merge", c("VRangesList", "missing"), function(x, y) {
-  ans <- unique(unlist(x, use.names=FALSE))
-  ad.sum <- 0L
-  td.sum <- 0L
-  rd.sum <- 0L
-  for (xi in as.list(x, use.names=FALSE)) {
-    m <- match(ans, xi)
-    td.sum <- td.sum + ifelse(is.na(m), 0L, totalDepth(xi)[m])
-    ad.sum <- ad.sum + ifelse(is.na(m), 0L, altDepth(xi)[m])
-    rd.sum <- rd.sum + ifelse(is.na(m), 0L, refDepth(xi)[m])
-  }
-  altDepth(ans) <- ad.sum
-  totalDepth(ans) <- td.sum
-  refDepth(ans) <- rd.sum
-  ans
-})
