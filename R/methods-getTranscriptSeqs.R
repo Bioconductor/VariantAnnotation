3f9e6c8ba1c8dd86b4e45774597e2025b0e87580
Valerie Obenchain <vobencha@fhcrc.org>
2015-04-28 15:58:37 +0000

    defunct getTranscriptSeqs()
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/VariantAnnotation@103202 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/methods-getTranscriptSeqs.R
+++ R/methods-getTranscriptSeqs.R
@@ -4,51 +4,16 @@
 
 setMethod("getTranscriptSeqs",  c("GRangesList", "ANY"),
     function(query, subject, ...)
-    {
-    extractTranscriptSeqs(subject, query)
-    }
+        .Defunct("extractTranscriptSeqs")
 )
 
 setMethod("getTranscriptSeqs",  c("GRangesList", "FaFile"),
     function(query, subject, ...)
-    {
-        ## order by exon rank, check for mixed chromosomes, mixed strand 
-        txlist <- GenomicFeatures:::.makeUCSCTxListFromGRangesList(query,
-            decreasing.rank.on.minus.strand=FALSE)
-
-        ## back to GRanges for getSeq method 
-        widthEL <- elementLengths(query)
-        gr <- GRanges(
-                seqnames=Rle(txlist$chrom, widthEL), 
-                ranges=IRanges(start=unlist(txlist$exonStarts), 
-                               end=unlist(txlist$exonEnds)), 
-                strand=Rle(txlist$strand, widthEL))
-
-        seqs <- callGeneric(gr, subject, ...)
-        oneseq <- unlist(seqs)
-        widthView <- sum(width(query)) 
-        vws <- successiveViews(oneseq, widthView)
-        txseq <- as(vws, "DNAStringSet") 
-        names(txseq) <- names(query) 
-        txseq
-    }
+        .Defunct("extractTranscriptSeqs")
 )
  
 setMethod("getTranscriptSeqs",  c("GRanges", "FaFile"),
     function(query, subject, ...)
-    {
-        fa <- open(subject)
-        idx <- scanFaIndex(fa)
-        seqmatch <- seqlevels(query) %in% seqlevels(idx) 
-        if (any(seqmatch == FALSE)){
-            missingSeq <- seqlevels(subject)[seqmatch == FALSE]
-            stop(paste("sequence ", missingSeq, 
-                " not found in 'query' \n", sep=""))
-        }
- 
-        dna <- getSeq(fa, query) 
-        close(fa)
-        dna 
-    }
+        .Defunct("extractTranscriptSeqs")
 )
 
