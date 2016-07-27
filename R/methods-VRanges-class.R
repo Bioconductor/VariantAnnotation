37485f6550a8bb2c5dd11bc1051198d490d99a73
Michael Lawrence <lawremi@iastate.edu>
2016-07-27 13:29:10 +0000

    use [["FT"]] to avoid picking up FTZ field
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/VariantAnnotation@119679 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/methods-VRanges-class.R
+++ R/methods-VRanges-class.R
@@ -249,8 +249,8 @@ setAs("VCF", "VRanges", function(from) {
     totalDepth <- meta$DP
     meta$DP <- NULL
   }
-  if (!is.null(geno(from)$FT))
-    filter <- cbind(filter, parseFilterStrings(as.vector(geno(from)$FT)))
+  if (!is.null(geno(from)[["FT"]]))
+    filter <- cbind(filter, parseFilterStrings(as.vector(geno(from)[["FT"]])))
   otherGeno <- geno(from)[setdiff(names(geno(from)), c("AD", "DP", "FT"))]
   if (length(otherGeno) > 0L)
     meta <- DataFrame(meta, lapply(otherGeno, genoToMCol))
