f96cb74faa7674c2d0553b32ec2d86e2b9e40a67
Hervé Pagès <hpages@fredhutch.org>
2018-01-23 16:29:21 -0800

    update test for vcfWhich() setter

--- inst/unitTests/test_ScanVcfParam-class.R
+++ inst/unitTests/test_ScanVcfParam-class.R
@@ -4,10 +4,8 @@ test_ScanVcfParam_which <- function()
                          seq2=IRanges(c(100, 1000), c(1000, 2000)))
     svp <- ScanVcfParam(which=which)
     checkIdentical(which, vcfWhich(svp))
-    vcfWhich(svp) <- DataFrame()
-    checkTrue(is(vcfWhich(svp), "IntegerRangesList"))
-    vcfWhich(svp) <- SimpleList()
-    checkTrue(is(vcfWhich(svp), "IntegerRangesList"))
+    checkException(vcfWhich(svp) <- DataFrame(), silent=TRUE)
+    checkException(vcfWhich(svp) <- SimpleList(), silent=TRUE)
 }
 
 test_ScanVcfParam_fixed <- function()
